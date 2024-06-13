9bf6352c95ea238392caf13d1e7e4086ecf7ec95
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-13 23:12:15 +0200

    1

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,7 +1,15 @@
 import css from "./ErrorMessage.module.css";
 
 const ErrorMessage = () => {
-  return <div className={css.error}>ErrorMessage</div>;
+  return (
+    <>
+      <p>
+        <b className={css.error}>
+          Whoops, something went wrong! Please try reloading this page!
+        </b>
+      </p>
+    </>
+  );
 };
 
 export default ErrorMessage;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,19 @@
 import css from "./SearchBar.module.css";
 
 const SearchBar = () => {
-  return <div className={css.searchBar}>SearchBar</div>;
+  return (
+    <header>
+      <form className={css.form}>
+        <input
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
 };
 
 export default SearchBar;
